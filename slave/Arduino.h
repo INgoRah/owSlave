03c31a0a1a0cd2a4ce211b5a870042665faf4bdb
INgo Rah <INgo.Rah@intel.com>
2018-01-08 09:16:49 +0100

    Version Update with more decent commands and ATMEGA support
    - Introducing some more abstractions for ATMEGA and ATTINY reusing from Arduino
    - Introduce familiy compile switch
    - Abstraction interface for pinSetup
    - Reuse timer tick concept from Arduino (but use timer1)
    - Application command structure changed
    - Supporting input button with debouncing. Fires and interrupt on OW line after releasing the button
    - Sleep support if not button is used and no ongoing conversion and silence on the OW line
    - CRC generated on the fly no longer needed in owid initially
    - Move and verify (on atmega) pin stuff to wiring
    - New button implementation

--- slave/Arduino.h
+++ slave/Arduino.h
@@ -5,7 +5,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/sleep.h>
 #include <avr/pgmspace.h>
+#include <avr/eeprom.h>
 
 #define HIGH 0x1
 #define LOW  0x0
@@ -14,19 +16,29 @@
 #define OUTPUT 0x1
 #define INPUT_PULLUP 0x2
 
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
 #ifdef __cplusplus
 extern "C"{
 #endif
 
+void init(void);
 void pinMode(uint8_t, uint8_t);
 void digitalWrite(uint8_t, uint8_t);
+int digitalRead(uint8_t);
 /*
 int digitalRead(uint8_t);
 int analogRead(uint8_t);
 void analogReference(uint8_t mode);
 void analogWrite(uint8_t, int);
 */
-
+unsigned long millis(void);
+unsigned long micros(void);
 void delay(unsigned long);
 void delayMicroseconds(unsigned int us);
   
--- slave/add_env.bat
+++ slave/add_env.bat
@@ -1,5 +1,6 @@
-rem "C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8\avr8-gnu-toolchain\bin\avr-gcc.exe" --version
-rem "C:\Program Files (x86)\Arduino\hardware\tools\avr\bin\avr-gcc.exe" --version
-set PATH=D:\tools\Arduino\hardware\tools\avr\bin\;%PATH%
-
-rem set PATH=C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8\avr8-gnu-toolchain\bin;%PATH%
+rem set PATH=D:\tools\Arduino\hardware\tools\avr\bin\;%PATH%
+rem set PATH=C:\Program Files (x86)\\Arduino\hardware\tools\avr\bin;%PATH%
+@set PATH=D:\tools\Arduino\hardware\avr\bin;%PATH%
+avr-gcc.exe --version
+@echo make OW_FAMILY=A8
+@echo make OW_FAMILY=A3
