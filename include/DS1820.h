53a2371dfaa815e984849077f2017844567cb07f
INgo Rah <IRah@maxlinear.com>
2020-12-18 06:09:23 +0100

    Dual ROM working with alarm only on the DS2408 Remove owid read from asm code (done in C different location)

---
+++ include/DS1820.h
@@ -0,0 +1,17 @@
+#ifndef DS1820_H
+#define DS1820_H
+
+typedef union {
+	volatile uint8_t bytes[8];
+	struct {
+		int16_t temp;  //0
+		uint8_t TH;  //2
+		uint8_t TL;  //3
+		uint8_t config;  //4
+		uint8_t rrFF; //5
+		uint8_t rr00; //6
+		uint8_t rr10; //7
+	};
+} packt_t;
+
+#endif /* DS1820_H */
